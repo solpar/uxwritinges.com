cabbc79b08e3c7df425561ba9c9ecab6d1ccb1ae
Marcelo Zapaia <mzapaia@ints-MacBook-Pro.local>
2017-12-26 07:26:23 -0300

    vp

--- pages/_document.js
+++ pages/_document.js
@@ -6,13 +6,16 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <meta name="theme-color" content="#ffffff" />
+
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" />
+
           <link rel="apple-touch-icon" sizes="120x120" href="/static/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
           <link rel="manifest" href="/static/manifest.json" />
           <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5" />
-          <meta name="theme-color" content="#ffffff" />
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" />
         </Head>
         <body>
           <Main />
