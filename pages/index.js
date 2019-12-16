7084dc12b42d915d7e19068dcf498050ee753db0
Marcelo Zapaia <marcelozapaia@MB-4869.local>
2019-12-16 04:36:57 -0200

    fixes

--- pages/index.js
+++ pages/index.js
@@ -1,73 +1,83 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default () => (
   <div>
     <Head>
       <title>UX Writing en español</title>
-      <meta content="123"/>
-  <meta name=”Description” content=”Primera comunidad de UX writers en español.” />
-  <meta http-equiv=”Content-Language” content=”es”/>
-  <meta name="keywords" content="uxwriting, ux writing en español, argentina"/>
+      <meta content="123" />
+      <meta
+        name="Description"
+        content="Primera comunidad de UX writers en español."
+      />
+      <meta http-equiv="Content-Language" content="es" />
+      <meta
+        name="keywords"
+        content="uxwriting, ux writing en español, argentina"
+      />
     </Head>
     <div className="links">
-      <a href="https://www.linkedin.com/company/ux-writing-en-español"
-         target="_blank"
-         className="animated swing">
+      <a
+        href="https://www.linkedin.com/company/ux-writing-en-español"
+        target="_blank"
+        className="animated swing"
+      >
         <img src="/static/logos/lnkd.png" />
       </a>
-      <a href="https://www.instagram.com/uxwritinges"
-         target="_blank"
-         className="animated swing">
+      <a
+        href="https://www.instagram.com/uxwritinges"
+        target="_blank"
+        className="animated swing"
+      >
         <img src="/static/logos/ig.png" />
       </a>
-      <a href="https://medium.com/ux-writing-en-espanol"
-         target="_blank"
-         className="animated swing">
+      <a
+        href="https://medium.com/ux-writing-en-espanol"
+        target="_blank"
+        className="animated swing"
+      >
         <img src="/static/logos/medium.png" />
-    </a>
-      <a href="https://open.spotify.com/show/5fcpIYfbLA984haw5MicR1"
-         target="_blank"
-         className="animated swing">
+      </a>
+      <a
+        href="https://open.spotify.com/show/5fcpIYfbLA984haw5MicR1"
+        target="_blank"
+        className="animated swing"
+      >
         <img src="/static/logos/spotify.png" />
       </a>
-      </a>
     </div>
-    
 
     <style global jsx>{`
-      *{
-        box-sizing:border-box;
+      * {
+        box-sizing: border-box;
       }
-      body{
-        margin:0;
-        background-color:#B1E5D5;
-        color:#fafafa;
-        font-size:22px;
-      }
-
-      .links{
-        display:flex;
-        min-height:100vh;
-        align-items:center;
-        justify-content:center;
-        width:100%;
+      body {
+        margin: 0;
+        background-color: #b1e5d5;
+        color: #fafafa;
+        font-size: 22px;
       }
 
-      a{
-        color:#ddd;
-        text-decoration:none;
-        font-family: 'Bungee', cursive;
-        margin:12px;
-        transition:all .2s;
-      }
-      a:hover{
-
+      .links {
+        display: flex;
+        min-height: 100vh;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
       }
 
-      img{
-        height:50px;
+      a {
+        color: #ddd;
+        text-decoration: none;
+        font-family: "Bungee", cursive;
+        margin: 12px;
+        transition: all 0.2s;
+      }
+      a:hover {
       }
 
+      img {
+        height: 50px;
+      }
     `}</style>
   </div>
-)
+);
